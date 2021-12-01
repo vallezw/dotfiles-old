79d2590cc26f9aef5bca35c708ca4b05a6b9b933
vallezw <v.zwerschke@mail.de>
2021-12-01 22:03:25 +0100

    Add doom emacs config

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/rsblocks/rsblocks.yml ~/.config/rsblocks/rsblocks.yml
+ln -s -f ~/dotfiles/.doom.d/config.el ~/.doom.d/config.el
+ln -s -f ~/dotfiles/.doom.d/init.el ~/.doom.d/init.el
+ln -s -f ~/dotfiles/.doom.d/packages.el ~/.doom.d/packages.el
